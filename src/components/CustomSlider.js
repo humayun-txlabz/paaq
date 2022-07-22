802b1a163e1f9abc989ac00d08d1bdcd97a40fd8
Ali Hussain Kazmi <ali.hussain@txlabz.com>
2022-07-22 12:32:53 +0500

    Animations of Progress Bars in skill Sections have been done

--- src/components/CustomSlider.js
+++ src/components/CustomSlider.js
@@ -1,23 +1,34 @@
-import Image from 'next/image';
+
+import React, {  useState, useLayoutEffect, useEffect } from 'react'
 // import { Slider } from 'theme-ui';
 import { Slider, Switch } from 'antd';
+import useIsInViewport from 'use-is-in-viewport'
 
-const CustomSlider = (props) => {
-
+const CustomSlider = React.forwardRef(function CustomSlider(props, parentRef){
+    const [isDivInViewport, progressBarReference] = useIsInViewport({
+        target: parentRef
+      })
+    const [value, setValue] = useState(0);
     const { item, index } = props;
     
+    useEffect(()=>{
+        isDivInViewport ?  setValue(item.defaultValue)  : null
+    },[isDivInViewport])
+    
+   
     return (
-        <div className='skills-banner-slider-container' style={{ alignSelf: index%2 === 0 ? 'flex-start' : 'flex-end'}}>
+       
+        <div ref={progressBarReference}  className='skills-banner-slider-container' style={{ alignSelf: index%2 === 0 ? 'flex-start' : 'flex-end'}}>
                 <div className='skills-banner-slider-head'>
                     <span className='skills-banner-subheading'>{item.title}</span>
                     <span className='skills-banner-rank'>Skills: {item.defaultValue/10}/10</span>
                 </div>
-                <div className='skills-banner-slider' style={{ ['--sliderColor']: item.color }}>
+                <div className='skills-banner-slider' style={{ ['--sliderColor']: item.color, }}>
                     {/* {console.log('this isss itt', item.defaultValue)} */}
-                    <Slider defaultValue={item.defaultValue} tooltipVisible={false} />
+                    <Slider handleStyle={{transition:'1.5s ease',transitionDelay:'0.5s'}} value={value}  tooltipVisible={false}   />
                 </div>
         </div>
     )
-}
+})
 
 export default CustomSlider;

a1da21cf534b1ab6add1bd5de14de2075f87f6fe
Junaid Younas <junaidammar2013@gmail.com>
2022-09-29 11:00:39 +0500

    section done

--- src/components/Featues/index.js
+++ src/components/Featues/index.js
@@ -1,4 +1,6 @@
+import ShareKnowledgeSection from 'components/ShareKnowledgeSection'
 import React from 'react'
+import SectionFive from './sectionFive'
 import SectionFour from './sectionFour'
 import SectionOne from './sectionOne'
 import SectionThree from './sectionThree'
@@ -8,9 +10,11 @@ function MainFeatures() {
   return (
     <div className='features-page-container'>
         <SectionOne />
+        <SectionFive />
         <SectionTwo />
         <SectionThree />
         <SectionFour />
+        <ShareKnowledgeSection />
     </div>
   )
 }
--- src/components/Featues/sectionFour.js
+++ src/components/Featues/sectionFour.js
@@ -1,6 +1,6 @@
 import React from "react";
 const startIcon  = require('../../assets/Icons/feature-sec-4-breif.png');
-const sec11 = require('../../assets/Images/feature-sec-1-1.png');
+const sec11 = require('../../assets/Images/feature-sec-4-1.png');
 
 function SectionFour() {
   return (
@@ -10,7 +10,7 @@ function SectionFour() {
             <img src={startIcon} className="icon" />
             <div className="text">My Profession</div>
           </div>
-          <p>Having a delicious meal with a chilled cocktail on a sunny day or travelling the world with family and friends to our favourite locations is beautiful, but being rewarded for socialising and sharing your unique skills on PAAQ is even more attractive.</p>
+          <p>This feature makes the connection of users effective within the PAAQ community. Other users will be able to see your expertise and what you do for a living. Upon filling out your profile and describing what you do and sharing your achievements, our algorithm will suggest relevant contents and like-minded individuals within your field of specialisation. i.e., Photography, filmmaker, sculptor & artists of different kinds.</p>
       </div>
       <div className="right">
       <img src={sec11} className="image" />

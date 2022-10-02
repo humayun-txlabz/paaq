b60fdeac5a30e144792547e6abb128ba8bb5780d
codemili <junaidammar2013@gmail.com>
2022-10-02 14:21:59 +0500

    circles

--- src/components/Investors/sectionMarketSize.js
+++ src/components/Investors/sectionMarketSize.js
@@ -5,7 +5,12 @@ const SectionMarketSize = () => {
     <div className='investors-section-market-size'>
             <div className='section-market-size-main'>
                 <h1>Market Size</h1>
-                <div>
+                <div className="all-circles">
+                    <div className="circle one">
+                      <h3>5.7 Billion</h3>
+                    </div>
+                    <div className="circle two"></div>
+                    <div className="circle three"></div>
                     
                 </div>
             </div>

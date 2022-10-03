dc5ff281bee6f5fb4a0dab3879c29bf4706cc456
codemili <junaidammar2013@gmail.com>
2022-10-03 20:21:09 +0500

    market size shapes

--- src/components/Investors/sectionMarketSize.js
+++ src/components/Investors/sectionMarketSize.js
@@ -2,20 +2,31 @@ import React from 'react'
 
 const SectionMarketSize = () => {
   return (
-    <div className='investors-section-market-size'>
-            <div className='section-market-size-main'>
-                <h1>Market Size</h1>
-                <div className="all-circles">
-                    <div className="circle one">
-                      <h3>5.7 Billion</h3>
-                    </div>
-                    <div className="circle two"></div>
-                    <div className="circle three"></div>
-                    
-                </div>
+       <div className="investors-section-market-size">
+            <div className="section-market-size-main">
+                 <h1>Market Size</h1>
+                 <div className="all-circles">
+                      <div className="circle one">
+                           <h3>5.7 Billion</h3>
+                           <p>TAM Total <br/>Available Market</p>
+                      </div>
+                      <div className="circle two">
+                          <h3>4.4 Billion</h3>
+                           <p>SAM Serviceable<br/>Available Market</p>
+                      </div>
+                      <div className="circle three">
+                          <h3>45 Million</h3>
+                           <p>SOM Serviceable<br/>Obtainable Market</p>
+                      </div>
+                 </div>
+                 <h4>
+                      Embedded in human nature is the desire to learn, and as such,
+                      questions are asked daily by people willing to gain knowledge.
+                      Primarily, this is the market PAAQ is addressing.
+                 </h4>
             </div>
-    </div>
-  )
+       </div>
+  );
 }
 
 export default SectionMarketSize

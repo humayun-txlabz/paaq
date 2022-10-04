60c5c8df1b00295c40dcd65d7b9ebb51fb13a442
codemili <junaidammar2013@gmail.com>
2022-10-04 13:44:42 +0500

    feature page sections complete

--- src/components/miniCardWithIdentity.js
+++ src/components/miniCardWithIdentity.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
-const MiniCardWithIdentity = ({image, name, id, position}) => {
-  return (
-       <div className="mini-card-with-identity">
-            <img src={image} />
-            <h2>{name}</h2>
-            <p>{id}</p>
-            <h2>{position}</h2>
-            <div className="button-bottom">Follow</div>
-       </div>
-  );
-}
+const MiniCardWithIdentity = ({
+     image,
+     name,
+     imageComponent,
+     id,
+     position,
+     isButton,
+     marginNameTop,
+}) => {
+     const marginOnNameTop = marginNameTop ? { paddingTop: marginNameTop } : {};
+
+     return (
+          <div className="mini-card-with-identity">
+               <img src={image} />
+               {name ? <h2 style={marginOnNameTop}>{name}</h2> : null}
+               {id ? <p>{id}</p> : null}
+               {position ? <h2>{position}</h2> : null}
+               {isButton == false ? null : <div className="button-bottom">Follow</div>}
+          </div>
+     );
+};
 
 export default MiniCardWithIdentity

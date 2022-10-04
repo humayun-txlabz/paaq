17b781ae195487e813fd6eca6811d8ed62f8be40
codemili <junaidammar2013@gmail.com>
2022-10-04 11:18:49 +0500

    features my skills center component

--- src/components/Featues/mySkillImg/index.js
+++ src/components/Featues/mySkillImg/index.js
@@ -1,15 +1,70 @@
 import React from 'react'
+import ProgressComponent from '../../../components/progressComponent';
 
 const MySkillImageComponent = () => {
   return (
-    <div className="my-skill-img-component">
-          <div className="inner-component">
-                   <h1>My Skills</h1>
-                   <p>Skills shows the areas in which a person is good at something. People will feel more comfortable asking you questions if your skills are available for them to see. </p>
-                    <div className="h-line" />
-          </div>
-    </div>
-  )
+       <div className="my-skill-img-component">
+            <div className="inner-component">
+                 <h1>My Skills</h1>
+                 <p>
+                      Skills shows the areas in which a person is good at something.
+                      People will feel more comfortable asking you questions if your
+                      skills are available for them to see.{" "}
+                 </p>
+                 <div className="h-line" />
+                 <ProgressComponent
+                      progressColor={"#67AC5B"}
+                      title="Overall Skills"
+                      progress={40}
+                 />
+                 <div className="h-line" />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={70}
+                      marginBottom={1}
+                      progressColor={"#9DACAC"}
+                 />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={35}
+                      marginBottom={1}
+                      progressColor={"#9DACAC"}
+                      isMenu
+                 />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={45}
+                      marginBottom={1}
+                      progressColor={"#9DACAC"}
+                      isMenu
+                 />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={20}
+                      marginBottom={1}
+                      progressColor={"#9DACAC"}
+                      isMenu
+                 />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={40}
+                      marginBottom={1}
+                      progressColor={"#9DACAC"}
+                      isMenu
+                 />
+                 <ProgressComponent
+                      title="Overall Skills"
+                      progress={65}
+                      marginBottom={2}
+                      progressColor={"#9DACAC"}
+                      isMenu
+                 />
+            </div>
+            <div className="mini-left-badge">
+
+            </div>
+       </div>
+  );
 }
 
 export default MySkillImageComponent
--- src/components/progressComponent.js
+++ src/components/progressComponent.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import menuImg from '../assets/Icons/menu-black.png';
 
-const ProgressComponent = () => {
+const ProgressComponent = ({isMenu, progress, title, marginBottom, progressColor}) => {
+  const prog = progress ? {width: `${progress}%`} : {};
+  const marginB = marginBottom ? {marginBottom: `${marginBottom}rem`} : {};
+  const progColor = progressColor ? {background: progressColor} : {};
   return (
-    <div className="in-progress-component">
-          <h1>Overall Skill</h1>
-          <div className="progress-bg">
-                    
-          </div>
-    </div>
-  )
+       <div style={marginB} className="in-progress-component">
+            <h1>{title}</h1>
+            <div className="inner-progress">
+                 <div className="progress-bg">
+                      <div style={{ ...prog, ...progColor }} className="progress" />
+                 </div>
+                 {isMenu ? <img src={menuImg} /> : null}
+            </div>
+       </div>
+  );
 }
 
-export default MySkillImageComponent
+export default ProgressComponent;

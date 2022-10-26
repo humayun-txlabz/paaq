d6ac0714bf020acf67ff2a0ce1e33f925bd0bbf8
Junaid Younas <junaid.younas@txlabz.com>
2022-10-26 10:29:13 +0500

    pushed the changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfiguration = {
-  distDir: 'build',
-  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  // distDir: "_next",
+  assetPrefix: './',
+  // distDir: 'build',
+  // target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
   reactStrictMode: false,
   images: {
+    unoptimized: true,
     disableStaticImages: true,
     domains: ['images.ctfassets.net']
   },

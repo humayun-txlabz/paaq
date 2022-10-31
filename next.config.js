f1fb6e540d9653d325e64c2ff76ddea8ccce6376
Junaid Younas <junaid.younas@txlabz.com>
2022-10-31 14:58:50 +0500

    features page

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfiguration = {
   //   basePath: "/src",
   // },
   // basePath: "/src",
-  distDir: '.next',
-  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  // distDir: 'build',
+  // target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
   reactStrictMode: false,
   images: {
     unoptimized: true,

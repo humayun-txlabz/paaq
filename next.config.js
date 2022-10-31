c477a095861e37634ba1cdc0a4f1f4d4af4d3fe0
Junaid Younas <junaid.younas@txlabz.com>
2022-10-31 17:44:36 +0500

    push build

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfiguration = {
-  // distDir: "_next",
+  distDir: ".next",
   // assetPrefix: './',
   // publicRuntimeConfig: {
   //   basePath: "/src",
   // },
   // basePath: "/src",
-  distDir: 'build',
+  // distDir: 'build',
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
   reactStrictMode: false,
   images: {
@@ -18,7 +18,8 @@ const nextConfiguration = {
   },
   env: {
     CONTENTFUL_SPACE_ID: 'f35zh50bh0q8',
-    CONTENTFUL_ACCESS_TOKEN: 'HBb_zsbYezQhuwa9UdagqTWf46-2rXF8Fkqy4TrefbI'
+    CONTENTFUL_ACCESS_TOKEN: 'HBb_zsbYezQhuwa9UdagqTWf46-2rXF8Fkqy4TrefbI',
+    NETLIFY_NEXT_PLUGIN_SKIP: true
   },
 };
 

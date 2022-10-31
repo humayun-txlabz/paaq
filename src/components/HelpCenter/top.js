c0696f6110ec866b13b55905eabb9d0945b88312
Junaid Younas <junaid.younas@txlabz.com>
2022-10-31 10:46:48 +0500

    changes done

--- src/components/HelpCenter/top.js
+++ src/components/HelpCenter/top.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import RightImage from '../../assets/help-center-1.png';
 import SearchIcon from '../../assets/Icons/search-icon.png';
-
-const HelpCenterTop = () => {
+ 
+const HelpCenterTop = ({isAccountSettings}) => {
   return (
     <div className='grid-container-media-page-top'>
         <div className='left'>
+          <div className='aSBreadCrump'>{`Help Centre >`}  <span>Account Settings</span></div>
             <h2>How Can We Help You?</h2>
             <div className='input-with-search-icon'>
               <input placeholder='Search Here...' />

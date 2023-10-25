adb27f816e39acb75f20956cdd7b1b9a80d86a5e
SufianZulfiqar <sufian.zulfiqar@txlabz.com>
2023-10-25 21:51:23 +0500

    Ask Animation

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -1,17 +1,36 @@
+'use-client'
+
 import React from 'react'
 import { Image } from 'theme-ui';
 import ThumbsUpIcon from '../assets/Icons/thumbs-upp.svg';
 import ThumbsDownIcon from '../assets/Icons/thumbs-downn.svg';
+import { useState } from 'react';
+import feedbackimg from '../assets/Feedbackimg.png'
+// import '../../src/theme/styles.scss';
 
 const FeedbackCard = () => {
+  const [feedback, setFeedback] = useState(true)
+  
   return (
     <div className='feedback'>
         <div className='feedback-main'>
-            <p>Do you find this helpful?</p>
+            { feedback === true ? 
+              <>
+              <p>Do you find this helpful?</p>
             <div className='feedback-icon'>
-                <Image src={ThumbsUpIcon} />
-                <Image src={ThumbsDownIcon} />
+                <span onClick={()=>setFeedback(!feedback)}><Image src={ThumbsUpIcon} /></span>
+                <span onClick={()=>setFeedback(!feedback)}><Image src={ThumbsDownIcon} /></span>
             </div>
+              </> 
+            : 
+            <>
+            <div className='feedback-parent'>
+            <div className='feedback-like'>
+              <img src={feedbackimg} alt="" />
+            </div>
+            </div>
+            <p className='feedback-like-text'>Thank you for your feedback.</p>
+            </> }
         </div>
     </div>
   )
